8147a7878f0988419b6c60cc405eeb1d4815db89
sebastiantf <sebastian.tharakan97@gmail.com>
2019-08-20 00:28:35 +0530

    body: BatteryLevelPage()

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_battery_alarm/battery_level_page.dart';
 
 void main() {
   return runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(title: Text("Battery Alarm")),
+      body: BatteryLevelPage(),
     ),
   ));
 }
