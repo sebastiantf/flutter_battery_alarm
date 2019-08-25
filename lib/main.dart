bf428699489d31cc67616cf914fdfb11c0a8396d
sebastiantf <sebastian.tharakan97@gmail.com>
2019-08-25 13:35:26 +0530

    add title:

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:Battery_Alarm/battery_level_page.dart';
 
 void main() {
   return runApp(MaterialApp(
+    title: "Battery Alarm",
     home: Scaffold(
       appBar: AppBar(title: Text("Battery Alarm")),
       body: BatteryLevelPage(),
